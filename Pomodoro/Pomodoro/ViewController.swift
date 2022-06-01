3dfa0a6c3cc6705bd56de373673af51871795f48
Joonsang Yoo <wnstkd31@gmail.com>
2022-06-01 22:20:34 +0900

    part2-6. 뽀모도로

---
+++ Pomodoro/Pomodoro/ViewController.swift
@@ -0,0 +1,137 @@
+//
+//  ViewController.swift
+//  Pomodoro
+//
+//  Created by 유준상 on 2022/02/17.
+//
+
+import UIKit
+import AudioToolbox
+
+enum TimerStatus {
+    case start
+    case pause
+    case end
+}
+
+class ViewController: UIViewController {
+    @IBOutlet weak var timerLabel: UILabel!
+    @IBOutlet weak var progressView: UIProgressView!
+    @IBOutlet weak var datePicker: UIDatePicker!
+    @IBOutlet weak var cancelButton: UIButton!
+    @IBOutlet weak var toggleButton: UIButton!
+    @IBOutlet weak var tomatoImageView: UIImageView!
+    
+    var duration = 60
+    var timerStatus: TimerStatus = .end
+    var timer: DispatchSourceTimer?
+    var currentSeconds = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.configureTapEvent()
+        self.configureToggleButton()
+    }
+    
+    private func configureTapEvent() {
+        cancelButton.addTarget(self, action: #selector(tapCancelButton(_:)), for: .touchUpInside)
+        toggleButton.addTarget(self, action: #selector(tapToggleButton(_:)), for: .touchUpInside)
+    }
+    
+    private func configureToggleButton() {
+        self.toggleButton.setTitle("시작", for: .normal)
+        self.toggleButton.setTitle("일시정지", for: .selected)
+    }
+    
+    private func startTimer() {
+        if self.timer == nil {
+            self.timer = DispatchSource.makeTimerSource(flags: [], queue: .main)
+            self.timer?.schedule(deadline: .now(), repeating: 1) // 1초마다 반복
+            self.timer?.setEventHandler(handler: { [weak self] in
+                guard let self = self else { return }
+                self.currentSeconds -= 1
+                
+                let hour = self.currentSeconds / 3600
+                let minutes = (self.currentSeconds % 3600) / 60
+                let seconds = (self.currentSeconds % 3600) % 60
+                self.timerLabel.text = String(format: "%02d:%02d:%02d", hour, minutes, seconds)
+                self.progressView.progress = Float(self.currentSeconds) / Float(self.duration)
+                
+                UIView.animate(withDuration: 0.5, delay: 0, animations: {
+                    self.tomatoImageView.transform = CGAffineTransform(rotationAngle: .pi)
+                })
+                UIView.animate(withDuration: 0.5, delay: 0.5, animations: {
+                    self.tomatoImageView.transform = CGAffineTransform(rotationAngle: .pi * 2)
+                })
+                
+                if (self.currentSeconds) <= 0 {
+                    self.stopTimer()
+                    AudioServicesPlaySystemSound(1005)
+                }
+            })
+            self.timer?.resume()
+        }
+    }
+    
+    private func stopTimer() {
+        if self.timerStatus == .pause {
+            self.timer?.resume()
+        }
+        
+        self.timerStatus = .end
+        self.cancelButton.isEnabled = false
+        
+        UIView.animate(withDuration: 0.5, animations: {
+            self.timerLabel.alpha = 0
+            self.progressView.alpha = 0
+            self.datePicker.alpha = 1
+        })
+
+        self.toggleButton.isSelected = false
+        
+        self.timer?.cancel()
+        self.timer = nil
+    }
+    
+    @objc func tapCancelButton(_ button: UIButton) {
+        switch self.timerStatus {
+        case .start, .pause:
+            self.stopTimer()
+            
+        default:
+            break
+        }
+    }
+    
+    @objc func tapToggleButton(_ button: UIButton) {
+        self.duration = Int(self.datePicker.countDownDuration)
+        switch self.timerStatus {
+        case .end:
+            self.currentSeconds = self.duration
+            self.timerStatus = .start
+           
+            UIView.animate(withDuration: 0.5, animations: {
+                self.timerLabel.alpha = 1
+                self.progressView.alpha = 1
+                self.datePicker.alpha = 0
+            })
+            
+            self.toggleButton.isSelected = true
+            self.cancelButton.isEnabled = true
+            self.startTimer()
+            
+        case .start:
+            self.timerStatus = .pause
+            self.toggleButton.isSelected = false
+            self.timer?.suspend()
+            
+        case .pause:
+            self.timerStatus = .start
+            self.toggleButton.isSelected = true
+            self.timer?.resume()
+        }
+    }
+
+
+}
+
